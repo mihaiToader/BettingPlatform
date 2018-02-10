524cd5d66872071c8b5a80040c9401b06a21817e
Mihai Toader <toadermihai16@gmail.com>
2018-02-10 23:21:38 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## This is the awesome betting platform app s
+
+##### Made by Mihai Toader :smile:
+
---
+++ src/main/java/com/mkyong/domain/dto/UserDto.java
@@ -0,0 +1,46 @@
+package com.mkyong.domain.dto;
+
+/**
+ * Created by mtoader on 7/13/2017.
+ */
+public class UserDto {
+    private String name;
+    private String password;
+    private String repeatPassword;
+
+    public UserDto() {
+        name = "";
+        password = "";
+        repeatPassword = "";
+    }
+
+    public UserDto(String name, String password, String repeatPassword) {
+        this.name = name;
+        this.password = password;
+        this.repeatPassword = repeatPassword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
+
+    public void setRepeatPassword(String repeatPassword) {
+        this.repeatPassword = repeatPassword;
+    }
+}
